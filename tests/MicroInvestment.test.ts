f7f73704f8e095878cb6adce9dde080a21432abf
Henarubi <dhenarubi@gmail.com>
2025-01-28 22:53:07 +0100

    Update MicroInvestment.test.ts

--- tests/MicroInvestment.test.ts
+++ tests/MicroInvestment.test.ts
@@ -1,21 +1,72 @@
-
 import { describe, expect, it } from "vitest";
+import { Cl } from "@stacks/transactions";
 
 const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
+const investor1 = accounts.get("wallet_1")!;
+const investor2 = accounts.get("wallet_2")!;
+const business = accounts.get("wallet_3")!;
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
+describe("MicroInvestment", () => {
+    it("successfully registers a business", () => {
+        const registerCall = simnet.callPublicFn(
+            "MicroInvestment",
+            "register-business",
+            [],
+            business
+        );
+        expect(registerCall.result).toBeOk(Cl.bool(true));
 
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-  });
+        const businessInfo = simnet.callReadOnlyFn(
+            "MicroInvestment",
+            "get-business-info",
+            [Cl.principal(business)],
+            business
+        );
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+        expect(businessInfo.result).toStrictEqual({
+          type: 12, // Response type
+          data: {
+              'total-raised': {
+                  type: 1,
+                  value: 0n
+              },
+              'is-active': {
+                  type: 3
+              }
+          }
+        });
+
+    });
+    it("allows investment in a business", () => {
+        const investAmount = 1000;
+        const investCall = simnet.callPublicFn(
+            "MicroInvestment",
+            "invest",
+            [Cl.uint(investAmount), Cl.principal(business)],
+            investor1
+        );
+        expect(investCall.result).toBeOk(Cl.bool(true));
+
+        const investmentInfo = simnet.callReadOnlyFn(
+            "MicroInvestment",
+            "get-investment",
+            [Cl.principal(investor1)],
+            investor1
+        );
+
+        expect(investmentInfo.result).toStrictEqual({
+          type: 12,
+          data: {
+              amount: {
+                  type: 1,
+                  value: 1000n
+              },
+              'last-investment': {
+                  type: 1,
+                  value: 3n
+              }
+          }
+        });
+
+    });
 });
